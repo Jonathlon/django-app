be728a6c7778e75bfb71e047af75b86ec40a62aa
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2024-01-15 16:02:32 +0000

    Exercise 2.5

--- src/recipes/models.py
+++ src/recipes/models.py
@@ -1,12 +1,28 @@
 from django.db import models
+from django.shortcuts import reverse
 
 # Create your models here.
 class Recipe(models.Model):
     name = models.CharField(max_length = 120)    
     cooking_time = models.IntegerField(help_text = 'in mintues', default = 0 )    
-    difficulty = models.CharField(max_length = 13)    
     ingredients = models.CharField(max_length = 300)    
     description = models.TextField()
+    pictures = models.ImageField(upload_to = 'recipes', default = 'no_picture.jpg')
+
+    def calc_difficulty(self):
+        ingredients = self.ingredients.split(', ')
+        if self.cooking_time < 10 and len(ingredients) < 4:
+            difficulty = 'Easy'
+        elif self.cooking_time < 10 and len(ingredients) >= 4:
+            difficulty = 'Medium'
+        elif self.cooking_time >= 10 and len(ingredients) < 4:
+            difficulty = 'Intermediate'
+        elif self.cooking_time >= 10 and len(ingredients) >= 4:
+            difficulty = 'Hard'
+        return difficulty
     
     def __str__(self):
         return str(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('recipes:detail', kwargs={'pk': self.pk})
--- src/recipes/tests.py
+++ src/recipes/tests.py
@@ -4,12 +4,20 @@ from .models import Recipe
 # Create your tests here.
 
 class RecipeModelTest(TestCase):
-    def setUpTestData():
-        Recipe.objects.create(name = 'sandwich', cooking_time = 15, 
-                              difficulty = 'easy', ingredients = 'bread,ham,cheese', 
-                              description = 'this is the best sandwich of your life.' )
-    def test_recipe_name(self):
+    def test_cooking_time(self):
         recipe = Recipe.objects.get(id=1)
-        field_label = recipe._meta.get_field('name').verbose_name
-        
-        self.assertEqual(field_label, 'name')
+        recipe_cooking_time = recipe.cooking_time
+        self.assertEqual(recipe_cooking_time, 25)
+
+    def test_ingredients_list(self):
+        recipe = Recipe.objects.get(id=1)
+        recipe_ingredients = recipe.ingredients
+        self.assertEqual(recipe_ingredients, 'bread,ham,cheese,')
+
+    def test_get_absolute_url(self):
+        recipe = Recipe.objects.get(id=1)
+        self.assertEqual(recipe.get_absolute_url(), '/list/1')
+
+    def test_difficulty_calculation(self):
+        recipe = Recipe.objects.get(id=1)
+        self.assertEqual(recipe.calc_difficulty(), 'Intermediate')
