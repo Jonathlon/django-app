ac7eef7c72f980062304074b93b46b08869d4751
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2024-02-01 16:33:59 +0000

    Exercise 2.7

--- src/recipes/tests.py
+++ src/recipes/tests.py
@@ -4,36 +4,38 @@ from .forms import RecipeSearchForm
 
 # Create your tests here.
 
-from django.test import TestCase
-from .models import Recipe
-
-# Create your tests here.
 
 class RecipeModelTest(TestCase):
-    def test_cooking_time(self):
-        recipe = Recipe.objects.get(id=1)
-        recipe_cooking_time = recipe.cooking_time
-        self.assertEqual(recipe_cooking_time, 25)
+    def setUpTestData():
+        Recipe.objects.create(name="Pizza", ingredients="Dough, Tomato Paste, Mushrooms",
+                              cooking_time=20, description="	The best Pizza ever")
 
-    def test_ingredients_list(self):
+    def test_return_string(self):
         recipe = Recipe.objects.get(id=1)
-        recipe_ingredients = recipe.ingredients
-        self.assertEqual(recipe_ingredients, 'bread,ham,cheese,')
+        self.assertEqual(recipe.__str__(), "Recipe: Pizza")
+
+    def test_description(self):
+        recipe = Recipe.objects.get(id=1)
+        self.assertEqual(recipe.description,
+                         "The best Pizza ever")
+
+    def test_name_max_length(self):
+        recipe = Recipe.objects.get(name="Test Recipe")
+        max_length = recipe._meta.get_field('name').max_length
+        self.assertEqual(max_length, 120)
+
+    def test_ingredients_max_length(self):
+        recipe = Recipe.objects.get(name="Test Recipe")
+        max_length = recipe._meta.get_field('ingredients').max_length
+        self.assertEqual(max_length, 512)
 
     def test_get_absolute_url(self):
         recipe = Recipe.objects.get(id=1)
-        self.assertEqual(recipe.get_absolute_url(), '/list/1')
+        self.assertEqual(recipe.get_absolute_url(), "/recipes/list/1")
 
-    def test_difficulty_calculation(self):
-        recipe = Recipe.objects.get(id=1)
-        self.assertEqual(recipe.calc_difficulty(), 'Intermediate')
 
-class RecipeFormTestCase(TestCase):
-    def test_valid_form(self):
-        data = {
-            "name": "Spaghetti Bolognese",
-            "cooking_time": 25,
-            "ingredients": "Spaghetti, Beef Mince, Tomato Paste, Tomato Sauce, Onion",
-        }
-        form = RecipeSearchForm(data=data)
+class RecipeFormTest(TestCase):
+    def test_recipe_search_form_valid(self):
+        form_data = {'recipe_name': 'Test Recipe'}
+        form = RecipeSearchForm(data=form_data)
         self.assertTrue(form.is_valid())
--- src/users/tests.py
+++ src/users/tests.py
@@ -1,19 +1,19 @@
-from django.test import TestCase
-from .models import User, Recipe
+# from django.test import TestCase
+# from .models import User, Recipe
 
-# Create your tests here.
+# # Create your tests here.
 
-class UserModelTest(TestCase):
-    def setUpTestData():
-        recipe = Recipe.objects.create(name = 'sandwich', cooking_time = 15, 
-                              difficulty = 'easy', ingredients = 'bread,ham,cheese', 
-                              description = 'this is the best sandwich of your life.' )
-        recipe.save()
-        user = User.objects.create(name = 'jon', saved_recipes = recipe)
-        user.save()
+# class UserModelTest(TestCase):
+#     def setUpTestData():
+#         recipe = Recipe.objects.create(name = 'sandwich', cooking_time = 15, 
+#                               difficulty = 'easy', ingredients = 'bread,ham,cheese', 
+#                               description = 'this is the best sandwich of your life.' )
+#         recipe.save()
+#         user = User.objects.create(name = 'jon', saved_recipes = recipe)
+#         user.save()
         
-    def test_user_name(self):
-        user = User.objects.get(id=1)
-        field_label = user._meta.get_field('saved_recipes').verbose_name
+#     def test_user_name(self):
+#         user = User.objects.get(id=1)
+#         field_label = user._meta.get_field('saved_recipes').verbose_name
         
-        self.assertEqual(field_label, 'saved recipes')
+#         self.assertEqual(field_label, 'saved recipes')
