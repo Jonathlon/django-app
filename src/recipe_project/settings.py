4540ff02ed91232427a6a5e09e7af5e2ccf15ee7
Jon Heaney <113617821+Jonathlon@users.noreply.github.com>
2024-02-08 12:23:42 +0000

    Exercise 2.8

--- src/recipe_project/settings.py
+++ src/recipe_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -63,6 +64,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -121,15 +123,32 @@ LOGIN_URL='/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
-STATICFILES_DIRS=[
-   BASE_DIR / 'static'
-]
+# STATICFILES_DIRS=[
+#    BASE_DIR / 'static'
+# ]
 
+# MEDIA_URL = '/media/'
+
+# MEDIA_ROOT = BASE_DIR / 'media'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+ 
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
+ 
+# STATICFILES_STORAGE = 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_ROOT = BASE_DIR / 'media'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import home, records, create_view, about_view
 from .views import RecipeListView, RecipesDetailsView
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'recipes'
 
@@ -12,3 +15,6 @@ urlpatterns = [
     path('create/', create_view, name='create'),
     path('about/', about_view, name='about'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
